45c45558badb6601373d50c6787a2e5da345208e
okyuce <okyuce@gmail.com>
2025-12-02 11:51:29 +0300

    Fix migration down_revision to 6094d11a2370

--- backend/alembic/versions/20251202_update_product_display_order.py
+++ backend/alembic/versions/20251202_update_product_display_order.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '20251202_product_order'
-down_revision = 'fix_revision_diff_type'
+down_revision = '6094d11a2370'
 branch_labels = None
 depends_on = None
 
